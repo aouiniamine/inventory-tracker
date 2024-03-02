35fb5abe331d3868f1c254c2f37c35211308ca85
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-02 22:21:44 +0100

    setup save & get all

---
+++ src/main/java/com/my_inventory/my_inventory/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.my_inventory.my_inventory.service;
+
+import com.my_inventory.my_inventory.entity.Product;
+import com.my_inventory.my_inventory.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAll(){
+        return productRepository.findAll();
+    }
+}
